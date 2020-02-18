63916304848bf637db7a27923b1a635b06f0f7d6
mervinkogei <vincent.kogei@yahoo.com>
2020-02-18 21:58:26 +0300

    Create a Fragment package and a current Music Fragment

---
+++ app/src/main/java/com/example/swiftmusic/Fragments/CurrentMusic.java
@@ -0,0 +1,32 @@
+package com.example.swiftmusic.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.swiftmusic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CurrentMusic extends Fragment {
+
+
+    public CurrentMusic() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_current_music, container, false);
+    }
+
+}
