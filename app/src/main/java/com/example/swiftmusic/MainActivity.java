ba2b28e81c45061a543f615ce2877449f1c67086
mervinkogei <vincent.kogei@yahoo.com>
2020-02-18 21:52:36 +0300

    Bind the views to the MainActivity

--- app/src/main/java/com/example/swiftmusic/MainActivity.java
+++ app/src/main/java/com/example/swiftmusic/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.swiftmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    TabLayout mTabLayout;
+    TabItem currentMusic;
+    TabItem allMusic;
+    TabItem playlist;
+    ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Swift Music");
+
+        mTabLayout = findViewById(R.id.tabLayout);
+        currentMusic = findViewById(R.id.currentMusic);
+        allMusic= findViewById(R.id.allMusic);
+        playlist = findViewById(R.id.playlist);
+        mPager = findViewById(R.id.viewpager);
     }
 }
